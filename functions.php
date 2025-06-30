9db2816d763df052490af421cbebe05f27231c47
jamanul <jk.skb.25@gmail.com>
2025-06-30 16:06:13 +0600

    life took a new turn

--- functions.php
+++ functions.php
@@ -4,3 +4,13 @@
  * 
  * @package Aquila
  */
+
+
+function aq_enqueue_scripts()
+{
+    wp_enqueue_style("stylesheet", get_template_directory_uri() . '/style.css', [], filemtime(get_template_directory() . '/style.css'), 'all');
+    wp_enqueue_style("mainStylesheet", get_template_directory_uri() . '/assets/css/style.css', [], filemtime(get_template_directory() . '/assets/css/style.css'), 'all');
+}
+
+
+add_action("wp_enqueue_scripts", "aq_enqueue_scripts");
