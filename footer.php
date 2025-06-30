efb2e74563986038efd1be2f444f5364dc56f020
jamanul <jk.skb.25@gmail.com>
2025-06-30 13:33:19 +0600

    first commit

---
+++ footer.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * header Template
+ * 
+ * @package Aquila
+ * 
+ */
+?>
+
+<footer>
+    <h2>This is footer</h2>
+</footer>
+<?php wp_footer() ?>
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Main template file
+ *
+ * @package aquila
+ * 
+ */
+
+?>
+
+<?php
+get_header()
+    ?>
+<main>
+
+</main>
+
+<?php
+get_footer();
